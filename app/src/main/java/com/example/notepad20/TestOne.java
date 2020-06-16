09c03e221c8d22090934428829ce8ef88e8c4ec3
Fclever <43471385+fclecer@users.noreply.github.com>
2020-06-16 16:57:51 +0800

    Create TestOne.java

---
+++ app/src/main/java/com/example/notepad20/TestOne.java
@@ -0,0 +1,200 @@
+package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}
+package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}package com.m1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal {
+
+    public static void runOne(List<Animal> list){
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void runTwo(List<? super Dog> list){
+        for (Object animal : list) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+class Dog extends Animal{}
